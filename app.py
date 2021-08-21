bff5cae264015aa27fd07b754d2315617c9cee22
mtk9090 <73513297+mtk9090@users.noreply.github.com>
2021-08-21 16:31:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ def index():
 
 
 
+@app.errorhandler(404)
+def error(e):
+    return '<h1>ocorreu um error, Tente Mais tarde</h1>'
+
+
 
 if __name__ == '__main__':
     import os
@@ -24,9 +29,5 @@ if __name__ == '__main__':
     port  = int(os.environ.get("PORT", 5000))
  
     app.run(host, port)
-
-@app.errorhandler(404)
-def error(e):
-    return '<h1>ocorreu um error, Tente Mais tarde</h1>'
 
 '''App complet'''
